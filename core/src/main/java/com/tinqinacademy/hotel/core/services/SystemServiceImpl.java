566512e72cebc442abc650d32c73397f47d4b413
martinfilkov <martinfilkov1@gmail.com>
2024-07-29 12:40:43 +0300

    initial commit

--- core/src/main/java/com/tinqinacademy/hotel/core/services/SystemServiceImpl.java
+++ core/src/main/java/com/tinqinacademy/hotel/core/services/SystemServiceImpl.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.jsonpatch.mergepatch.JsonMergePatch;
 import com.tinqinacademy.hotel.api.operations.exception.InvalidInputException;
 import com.tinqinacademy.hotel.api.operations.exception.NotFoundException;
+import com.tinqinacademy.hotel.api.operations.hotel.bookroom.BookRoomInput;
+import com.tinqinacademy.hotel.api.operations.hotel.unbookroom.UnbookRoomInput;
 import com.tinqinacademy.hotel.api.operations.system.createroom.CreateRoomInput;
 import com.tinqinacademy.hotel.api.operations.system.createroom.CreateRoomOutput;
 import com.tinqinacademy.hotel.api.operations.system.deleteroom.DeleteRoomInput;
@@ -56,7 +58,6 @@ public class SystemServiceImpl implements SystemService {
     private final GuestRepository guestRepository;
     private final ConversionService conversionService;
     private final ObjectMapper mapper;
-    private final EntityManager entityManager;
 
     @Autowired
     public SystemServiceImpl(RoomRepository roomRepository, BedRepository bedRepository, GuestRepository guestRepository, ReservationRepository reservationRepository, ConversionService conversionService, ObjectMapper mapper, EntityManager entityManager) {
@@ -66,20 +67,49 @@ public class SystemServiceImpl implements SystemService {
         this.guestRepository = guestRepository;
         this.conversionService = conversionService;
         this.mapper = mapper;
-        this.entityManager = entityManager;
     }
 
     @Override
     public RegisterVisitorOutput registerVisitor(RegisterVisitorInputList inputList) {
         log.info("Start registerVisitor input: {}", inputList);
 
+        checkIfVisitorsNotEmpty(inputList);
+        checkIfReservationPeriodIsValid(inputList);
+
+        List<Guest> guestList = inputList.getVisitors().stream()
+                .map(guest -> conversionService.convert(guest, Guest.class))
+                .toList();
+
+        List<Guest> allGuests = guestRepository.saveAll(guestList);
+
+        Reservation reservation = getIfReservationExists(inputList);
+        reservation.setGuests(allGuests);
+
+        reservationRepository.save(reservation);
+        RegisterVisitorOutput output = new RegisterVisitorOutput();
+
+        log.info("End registerVisitor output: {}", output);
+        return output;
+    }
+
+    private void checkIfVisitorsNotEmpty(RegisterVisitorInputList inputList) {
+        log.info("Check if visitors are empty");
         if (inputList.getVisitors().isEmpty()) {
             throw new InvalidInputException("Visitors is empty");
         }
+        log.info("Visitors not empty");
+    }
 
+    private void checkIfReservationPeriodIsValid(RegisterVisitorInputList inputList) {
+        log.info("Check if period is valid");
         if (inputList.getEndDate().isBefore(inputList.getStartDate())) {
             throw new InvalidInputException("Start date cannot be after end date");
         }
+        log.info("Period is valid");
+    }
+
+    private Reservation getIfReservationExists(RegisterVisitorInputList inputList) {
+        log.info("Try to get reservation with room number: {}", inputList.getRoomNumber());
 
         Optional<Reservation> reservationOptional = reservationRepository.findAvailableRoomByRoomNumberAndPeriod(
                 inputList.getRoomNumber(),
@@ -93,20 +123,8 @@ public class SystemServiceImpl implements SystemService {
             );
         }
 
-        List<Guest> guestList = inputList.getVisitors().stream()
-                .map(guest -> conversionService.convert(guest, Guest.class))
-                .toList();
-
-        List<Guest> allGuests = guestRepository.saveAll(guestList);
-
-        Reservation reservation = reservationOptional.get();
-        reservation.setGuests(allGuests);
-
-        reservationRepository.save(reservation);
-        RegisterVisitorOutput output = new RegisterVisitorOutput();
-
-        log.info("End registerVisitor output: {}", output);
-        return output;
+        log.info("Found reservation with room number: {}", inputList.getRoomNumber());
+        return reservationOptional.get();
     }
 
     @Override
@@ -149,16 +167,9 @@ public class SystemServiceImpl implements SystemService {
     public CreateRoomOutput createRoom(CreateRoomInput input) {
         log.info("Start createRoom input: {}", input);
 
-        if (BathroomType.getByCode(input.getBathRoomType()).equals(BathroomType.UNKNOWN)) {
-            throw new NotFoundException("Bathroom type " + input.getBathRoomType() + " not found");
-        }
+        checkIfBathroomIsValid(input);
 
-        input.getBedSizes().forEach(bedSize ->
-        {
-            if (BedSize.getByCode(bedSize).equals(BedSize.UNKNOWN)) {
-                throw new NotFoundException("Bed size " + bedSize + " not found");
-            }
-        });
+        input.getBedSizes().forEach(this::checkIfBedSizeIsValid);
 
         Room room = conversionService.convert(input, Room.RoomBuilder.class)
                 .bedSizes(input.getBedSizes().stream().map(bed ->
@@ -175,32 +186,37 @@ public class SystemServiceImpl implements SystemService {
         return output;
     }
 
+    private void checkIfBathroomIsValid(CreateRoomInput input){
+        log.info("Check if bathroom type is valid");
+        if (BathroomType.getByCode(input.getBathRoomType()).equals(BathroomType.UNKNOWN)
+                && input.getBathRoomType() != null) {
+            throw new NotFoundException("Bathroom type " + input.getBathRoomType() + " not found");
+        }
+        log.info("Bathroom type is valid");
+    }
+
+    private void checkIfBedSizeIsValid(String bedSize){
+        if (BedSize.getByCode(bedSize).equals(BedSize.UNKNOWN)) {
+            throw new NotFoundException("Bed size " + bedSize + " not found");
+        }
+    }
+
     @Override
     public UpdateRoomOutput updateRoom(UpdateRoomInput input) {
         log.info("Start updateRoom input: {}", input);
 
-        Optional<Room> roomOptional = roomRepository.findById(UUID.fromString(input.getRoomId()));
-        if (roomOptional.isEmpty()) {
-            throw new NotFoundException("Room with id " + input.getRoomId() + " not found");
-        }
+        Room currentRoom = getIfRoomExists(input);
 
-        if (BathroomType.getByCode(input.getBathRoomType()).equals(BathroomType.UNKNOWN)) {
-            throw new NotFoundException("Bathroom type " + input.getBathRoomType() + " not found");
-        }
+        checkIfBathroomIsValid(input);
 
-        input.getBedSizes().forEach(bedSize ->
-        {
-            if (BedSize.getByCode(bedSize).equals(BedSize.UNKNOWN)) {
-                throw new NotFoundException("Bed size " + bedSize + " not found");
-            }
-        });
+        input.getBedSizes().forEach(this::checkIfBedSizeIsValid);
 
         Room room = conversionService.convert(input, Room.RoomBuilder.class)
                 .bedSizes(input.getBedSizes().stream().map(bed ->
                                 bedRepository.findByBedSize(BedSize.getByCode(bed)).orElseThrow())
                         .toList()
                 )
-                .floor(roomOptional.get().getFloor())
+                .floor(currentRoom.getFloor())
                 .build();
 
         Room updatedRoom = roomRepository.save(room);
@@ -211,30 +227,37 @@ public class SystemServiceImpl implements SystemService {
         return output;
     }
 
-    @SneakyThrows
-    @Override
-    public PartialUpdateRoomOutput partialUpdateRoom(PartialUpdateRoomInput input) {
-        log.info("Start partialUpdateRoom input: {}", input);
+    private void checkIfBathroomIsValid(UpdateRoomInput input){
+        log.info("Check if bathroom type is valid");
+        if (BathroomType.getByCode(input.getBathRoomType()).equals(BathroomType.UNKNOWN)
+                && input.getBathRoomType() != null) {
+            throw new NotFoundException("Bathroom type " + input.getBathRoomType() + " not found");
+        }
+        log.info("Bathroom type is valid");
+    }
+
+    private Room getIfRoomExists(UpdateRoomInput input){
+        log.info("Try to get room with id: {}", input.getRoomId());
 
         Optional<Room> roomOptional = roomRepository.findById(UUID.fromString(input.getRoomId()));
         if (roomOptional.isEmpty()) {
             throw new NotFoundException("Room with id " + input.getRoomId() + " not found");
         }
 
-        if (BathroomType.getByCode(input.getBathRoomType()).equals(BathroomType.UNKNOWN)
-                && input.getBathRoomType() != null) {
-            throw new NotFoundException("Bathroom type " + input.getBathRoomType() + " not found");
-        }
+        log.info("Room with id {} exists", input.getRoomId());
+        return roomOptional.get();
+    }
 
-        if (input.getBedSizes() != null) {
-            input.getBedSizes().forEach(bedSize ->
-            {
-                if (BedSize.getByCode(bedSize).equals(BedSize.UNKNOWN)) {
-                    throw new NotFoundException("Bed size " + bedSize + " not found");
-                }
-            });
-        }
-        Room currentRoom = roomOptional.get();
+    @SneakyThrows
+    @Override
+    public PartialUpdateRoomOutput partialUpdateRoom(PartialUpdateRoomInput input) {
+        log.info("Start partialUpdateRoom input: {}", input);
+
+        checkIfBathroomIsValid(input);
+
+        checkIfBedSizeIsValid(input);
+
+        Room currentRoom = getIfRoomExists(input);;
 
         Room inputRoom = conversionService.convert(input, Room.RoomBuilder.class)
                 .bedSizes(input.getBedSizes() != null ?
@@ -257,21 +280,58 @@ public class SystemServiceImpl implements SystemService {
         return output;
     }
 
+    private Room getIfRoomExists(PartialUpdateRoomInput input){
+        log.info("Try to get room with id: {}", input.getRoomId());
+
+        Optional<Room> roomOptional = roomRepository.findById(UUID.fromString(input.getRoomId()));
+        if (roomOptional.isEmpty()) {
+            throw new NotFoundException("Room with id " + input.getRoomId() + " not found");
+        }
+
+        log.info("Room with id {} exists", input.getRoomId());
+        return roomOptional.get();
+    }
+
+    private void checkIfBathroomIsValid(PartialUpdateRoomInput input){
+        log.info("Check if bathroom type is valid");
+        if (BathroomType.getByCode(input.getBathRoomType()).equals(BathroomType.UNKNOWN)
+                && input.getBathRoomType() != null) {
+            throw new NotFoundException("Bathroom type " + input.getBathRoomType() + " not found");
+        }
+        log.info("Bathroom type is valid");
+    }
+
+    private void checkIfBedSizeIsValid(PartialUpdateRoomInput input){
+        log.info("Check if each bed size is valid and not null");
+        if (input.getBedSizes() != null) {
+            input.getBedSizes().forEach(this::checkIfBedSizeIsValid);
+        }
+        log.info("Bed sizes are valid");
+    }
+
     @Override
     public DeleteRoomOutput deleteRoom(DeleteRoomInput input) {
         log.info("Start deleteRoom input: {}", input);
 
-        Optional<Room> roomOptional = roomRepository.findById(UUID.fromString(input.getId()));
+        Room room = getIfRoomExists(input);
 
-        if (roomOptional.isEmpty()) {
-            throw new NotFoundException("Room with id " + input.getId() + " not found");
-        }
-
-        roomRepository.delete(roomOptional.get());
+        roomRepository.delete(room);
 
         DeleteRoomOutput output = new DeleteRoomOutput();
 
         log.info("End deleteRoom output: {}", output);
         return output;
+    }
+
+    private Room getIfRoomExists(DeleteRoomInput input){
+        log.info("Try to get room with id: {}", input.getId());
+
+        Optional<Room> roomOptional = roomRepository.findById(UUID.fromString(input.getId()));
+        if (roomOptional.isEmpty()) {
+            throw new NotFoundException("Room with id " + input.getId() + " not found");
+        }
+
+        log.info("Room with id {} exists", input.getId());
+        return roomOptional.get();
     }
 }
