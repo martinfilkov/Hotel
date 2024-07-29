74e5891a0fa856a0bd01960772b597eb012bdb6d
martinfilkov <martinfilkov1@gmail.com>
2024-07-29 11:00:36 +0300

    Fix formatting

--- core/src/main/java/com/tinqinacademy/hotel/core/specifications/GuestSpecifications.java
+++ core/src/main/java/com/tinqinacademy/hotel/core/specifications/GuestSpecifications.java
@@ -6,31 +6,45 @@ import org.springframework.data.jpa.domain.Specification;
 public class GuestSpecifications {
 
     public static Specification<Guest> guestHasFirstName(String firstName) {
-        return isValid(firstName) ? (guest, cq, cb) -> cb.equal(guest.get("firstName"), firstName) : (root, query, cb) -> cb.conjunction();
+        return isValid(firstName)
+                ? (guest, cq, cb) -> cb.equal(guest.get("firstName"), firstName)
+                : (root, query, cb) -> cb.conjunction();
     }
 
     public static Specification<Guest> guestHasLastName(String lastName) {
-        return isValid(lastName) ? (guest, cq, cb) -> cb.equal(guest.get("lastName"), lastName) : (root, query, cb) -> cb.conjunction();
+        return isValid(lastName)
+                ? (guest, cq, cb) -> cb.equal(guest.get("lastName"), lastName)
+                : (root, query, cb) -> cb.conjunction();
     }
 
     public static Specification<Guest> guestHasPhoneNumber(String phoneNumber) {
-        return isValid(phoneNumber) ? (guest, cq, cb) -> cb.equal(guest.get("phoneNumber"), phoneNumber) : (root, query, cb) -> cb.conjunction();
+        return isValid(phoneNumber)
+                ? (guest, cq, cb) -> cb.equal(guest.get("phoneNumber"), phoneNumber)
+                : (root, query, cb) -> cb.conjunction();
     }
 
     public static Specification<Guest> guestHasIdCardNumber(String idCardNumber) {
-        return isValid(idCardNumber) ? (guest, cq, cb) -> cb.equal(guest.get("idCardNumber"), idCardNumber) : (root, query, cb) -> cb.conjunction();
+        return isValid(idCardNumber)
+                ? (guest, cq, cb) -> cb.equal(guest.get("idCardNumber"), idCardNumber)
+                : (root, query, cb) -> cb.conjunction();
     }
 
     public static Specification<Guest> guestHasIdCardValidity(String idCardValidity) {
-        return isValid(idCardValidity) ? (guest, cq, cb) -> cb.equal(guest.get("idCardValidity"), idCardValidity) : (root, query, cb) -> cb.conjunction();
+        return isValid(idCardValidity)
+                ? (guest, cq, cb) -> cb.equal(guest.get("idCardValidity"), idCardValidity)
+                : (root, query, cb) -> cb.conjunction();
     }
 
     public static Specification<Guest> guestHasIdCardIssueAuthority(String idCardIssueAuthority) {
-        return isValid(idCardIssueAuthority) ? (guest, cq, cb) -> cb.equal(guest.get("idCardIssueAuthority"), idCardIssueAuthority) : (root, query, cb) -> cb.conjunction();
+        return isValid(idCardIssueAuthority)
+                ? (guest, cq, cb) -> cb.equal(guest.get("idCardIssueAuthority"), idCardIssueAuthority)
+                : (root, query, cb) -> cb.conjunction();
     }
 
     public static Specification<Guest> guestHasIdCardIssueDate(String idCardIssueDate) {
-        return isValid(idCardIssueDate) ? (guest, cq, cb) -> cb.equal(guest.get("idCardIssueDate"), idCardIssueDate) : (root, query, cb) -> cb.conjunction();
+        return isValid(idCardIssueDate)
+                ? (guest, cq, cb) -> cb.equal(guest.get("idCardIssueDate"), idCardIssueDate)
+                : (root, query, cb) -> cb.conjunction();
     }
 
     private static boolean isValid(String value) {
